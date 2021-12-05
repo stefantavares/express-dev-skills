af6f19cbdc411e39fc437e7aca85af3b2d5daeea
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-12-04 23:52:13 -0800

    Add some css changes

--- models/skill.js
+++ models/skill.js
@@ -1,6 +1,6 @@
 const skills = [
-    { id: 1, skill: 'Make the best web applications ever.', description: 'Honestly he is the greatest at that.' },
-    { id: 2, skill: 'Make the coolest games ever.', description: 'Honestly, really really cool games.' },
+    { id: 1, skill: 'Make the best web applications ever.', description: 'Honestly, he is the greatest at that.' },
+    { id: 2, skill: 'Make the coolest games ever.', description: 'Honestly, like really really cool games.' },
     { id: 3, skill: 'Hermit for 12 days straight.', description: 'Who needs human interaction anyways?' },
     { id: 4, skill: 'Hack into the Pentagon.', description: 'Would prefer to not go to jail, but willing to for the right price.' },
     { id: 5, skill: 'Make above average money.', description: "Money can't buy happiness, but it sure helps" }
