89424e0f244b5009addd2c76873be0107ee620f0
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-12-04 21:21:04 -0800

    Add skills to /skills page as a link

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var skillsCtrl = require('../controllers/skills');
+
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
